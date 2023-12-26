414f60ba65d3aa76af07447737f0e54442f27045
Aptemond <sofartem5@gmail.com>
2023-12-26 20:40:16 +0500

    add Create view with OTP

--- users/views.py
+++ users/views.py
@@ -17,6 +17,22 @@ from .serializers \
 logger = logging.getLogger(__name__)
 
 
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        phone = request.data.get('phone')
+        if not phone:
+            return Response({'error': 'Phone number is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        otp_code = get_random_string(length=4, allowed_chars='1234567890')
+        user = CustomUser(phone=phone, one_time_password=otp_code)
+        user.invite_code = generate_unique_invite_code()
+        user.is_active = False
+        user.save()
+
+        return Response({'message': 'OTP code sent successfully'}, status=status.HTTP_200_OK)
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
@@ -65,3 +81,50 @@ class UserDeleteAPIView(generics.DestroyAPIView):
         self.perform_destroy(instance)
         return Response({"message": f"Пользователь {instance.phone} удален"}, status=status.HTTP_204_NO_CONTENT)
 
+def generate_unique_invite_code():
+    while True:
+        invite_code = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(6))
+        if not CustomUser.objects.filter(invite_code=invite_code).exists():
+            return invite_code
+
+
+class UserCodeAccept(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegisterCodeAcceptSerializer
+
+    def create(self, request, *args, **kwargs):
+        code = request.data.get('one_time_password')
+        user = CustomUser.objects.get(phone=request.data.get('phone'))
+        if not user.is_active:
+            if code == user.one_time_password:
+                password = CustomUser.objects.make_random_password()
+                user.set_password(password)
+                user.is_active = True
+                user.save()
+                return Response(
+                    {'password': password},
+                    status=status.HTTP_200_OK)
+            return Response({'error': 'Неправильный пароль'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'Аккаунт уже активирован'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInviteCode(generics.UpdateAPIView):
+    serializer_class = UserInviteCodeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        user = request.user  # Получение текущего авторизованного пользователя
+        invite_code = request.data.get('activated_invite_code')
+        print(invite_code)
+        if user.activated_invite_code:
+            return Response({'message': f"Invite code already activated: "
+                                        f"{user.activated_invite_code}"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        # Обновление информации о пользователе
+        user.activated_invite_code = invite_code
+        user.save()
+        return Response({'message': f"Invite code activated successfully "
+                                    f"{invite_code}"},
+                        status=status.HTTP_200_OK)
