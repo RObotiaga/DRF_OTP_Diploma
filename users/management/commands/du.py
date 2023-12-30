7751d807304cce754a85d11deaaa8afd9c4a1f42
Aptemond <sofartem5@gmail.com>
2023-12-30 15:51:40 +0500

    change create/upload to perform_create/upload

--- users/management/commands/du.py
+++ users/management/commands/du.py
@@ -1,19 +1,25 @@
 from django.core.management import BaseCommand
 from users.models import CustomUser
 
+
 class Command(BaseCommand):
     help = 'Delete a user by phone number'
 
     def add_arguments(self, parser):
-        parser.add_argument('phone', type=str, help='Phone of the user to delete')
+        parser.add_argument('phone',
+                            type=str,
+                            help='Phone of the user to delete')
 
     def handle(self, *args, **options):
         phone = options['phone']
         try:
             user_to_delete = CustomUser.objects.get(phone=phone)
             user_to_delete.delete()
-            self.stdout.write(self.style.SUCCESS('Пользователь успешно удален'))
+            self.stdout.write(
+                self.style.SUCCESS('Пользователь успешно удален'))
         except CustomUser.DoesNotExist:
-            self.stdout.write(self.style.ERROR('Пользователь не найден'))
+            self.stdout.write(
+                self.style.ERROR('Пользователь не найден'))
         except Exception as e:
-            self.stdout.write(self.style.ERROR(f'Ошибка при удалении пользователя: {e}'))
+            self.stdout.write(
+                self.style.ERROR(f'Ошибка при удалении пользователя: {e}'))
